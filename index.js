23f707213b138a376a50a8a13b015566d2e34bfa
bghalami <benjaminghalami@gmail.com>
2018-11-28 23:15:00 -0700

    made login button disabled if no password and username

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const createAccountButton = document.querySelector(".create-account-button");
 const goBackButton        = document.querySelector(".go-back");
 const loginButton        = document.querySelector(".login-button");
 
+let usernameLogin   = document.querySelector(".username-login");
+let passwordLogin   = document.querySelector(".password-login");
+
 const accountCreate = document.querySelector(".account-create");
 const loginScreen   = document.querySelector(".login-screen");
 const playGame      = document.querySelector(".play-game");
@@ -9,10 +12,22 @@ const playGame      = document.querySelector(".play-game");
 accountCreate.style.display = "none";
 playGame.style.display = "none";
 
-// deactivateLogin();
+deactivateLogin();
+
+function unlockLogin() {
+  if ((usernameLogin.value) && (passwordLogin.value)) {
+    activateLogin();
+  } else {
+    deactivateLogin();
+  }
+}
 
 function deactivateLogin() {
   loginButton.className = "landing-button login-button disabled-button"
+}
+
+function activateLogin() {
+  loginButton.className = "landing-button login-button"
 }
 
 function createLoginSwitch() {
@@ -35,6 +50,9 @@ createAccountButton.addEventListener("click", createLoginSwitch);
 goBackButton.addEventListener("click", createLoginSwitch);
 loginButton.addEventListener("click", tryToLogin);
 
+usernameLogin.addEventListener("input", unlockLogin);
+passwordLogin.addEventListener("input", unlockLogin);
+
 function tryToLogin() {
   var request = new XMLHttpRequest();
   request.open('POST', 'https://bens-blackjack-switch.herokuapp.com/api/v1/login', true);
@@ -49,9 +67,6 @@ function tryToLogin() {
     }
     console.log("oo");
   }
-
-  let usernameLogin   = document.querySelector(".username-login");
-  let passwordLogin   = document.querySelector(".password-login");
 
   request.send(`username=${usernameLogin.value}&password=${passwordLogin.value}`);
   console.log("yo");
