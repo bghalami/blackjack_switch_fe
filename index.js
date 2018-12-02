fa3a7de1810da7e90c6f0b0e96d6f1dcd431da27
bghalami <benjaminghalami@gmail.com>
2018-12-02 15:49:02 -0700

    Switched from XML HTTP Request, to fetch. Finished log in

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const createAccountButton = document.querySelector(".create-account-button");
 const goBackButton        = document.querySelector(".go-back");
-const loginButton        = document.querySelector(".login-button");
+const loginButton         = document.querySelector(".login-button");
+const createUserButton    = document.querySelector(".create-user-button");
+
 
 let usernameLogin   = document.querySelector(".username-login");
 let passwordLogin   = document.querySelector(".password-login");
+let usernameCreate  = document.querySelector(".username-create");
+let passwordCreate  = document.querySelector(".password-create");
 
 const accountCreate = document.querySelector(".account-create");
 const loginScreen   = document.querySelector(".login-screen");
@@ -13,12 +17,21 @@ accountCreate.style.display = "none";
 playGame.style.display = "none";
 
 deactivateLogin();
+deactivateCreate();
 
 function unlockLogin() {
   if ((usernameLogin.value) && (passwordLogin.value)) {
     activateLogin();
   } else {
     deactivateLogin();
+  }
+}
+
+function unlockCreate() {
+  if ((usernameCreate.value) && (passwordCreate.value)) {
+    activateCreate();
+  } else {
+    deactivateCreate();
   }
 }
 
@@ -29,6 +42,16 @@ function deactivateLogin() {
 function activateLogin() {
   loginButton.className = "landing-button login-button"
 }
+
+function deactivateCreate() {
+  createUserButton.className = "landing-button create-user-button disabled-button"
+}
+
+function activateCreate() {
+  createUserButton.className = "landing-button create-user-button"
+}
+
+
 
 function createLoginSwitch() {
   if (loginScreen.style.display === "none") {
@@ -52,22 +75,32 @@ loginButton.addEventListener("click", tryToLogin);
 
 usernameLogin.addEventListener("input", unlockLogin);
 passwordLogin.addEventListener("input", unlockLogin);
+usernameCreate.addEventListener("input", unlockCreate);
+passwordCreate.addEventListener("input", unlockCreate);
 
 function tryToLogin() {
-  var request = new XMLHttpRequest();
-  request.open('POST', 'https://bens-blackjack-switch.herokuapp.com/api/v1/login', true);
-  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  event.preventDefault();
+  let data = { username: `${usernameLogin.value}`,
+              password: `${passwordLogin.value}`
+             }
+  const package = { method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(data)
+                  }
+  fetch('https://bens-blackjack-switch.herokuapp.com/api/v1/login', package)
+  .then(res => res.json())
+  .then(response => {
+    loginHandler(response);
+  });
+};
 
-  request.onreadystatechange = function() {
-    if (this.status === 202) {
-      letsPlay();
-    } else {
-      let alert = document.querySelector(".heads-up");
-      alert.innerHTML = "Something went wrong, please try again";
-    }
-    console.log("oo");
+function loginHandler(response) {
+  if(response.status === 202) {
+    console.log(response);
+    letsPlay();
+  } else {
+    console.log(response);
+    let alert = document.querySelector(".heads-up");
+    alert.innerHTML = "Something went wrong, please try again";
   }
-
-  request.send(`username=${usernameLogin.value}&password=${passwordLogin.value}`);
-  console.log("yo");
 }
